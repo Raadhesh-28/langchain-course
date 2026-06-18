d22bfe34d79d47319ea52c663d0d9f0f3fd66d86
Raadhesh-11 <raadhesh@softborne.com>
2026-06-18 18:45:18 +0530

    intro to create_agent functioning

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-load_dotenv ()
+load_dotenv()
 from langgraph.prebuilt import create_react_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
-from tavily import TavilyClient
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+#from langchain.agents import create_react_agent
 
+class Source (BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field (description="The url of the source")
 
-tavily = TavilyClient ()
-@tool
-def search (query:str) -> str:
-   """
-   Tool that searches over the internet
-   Args:
-   query: The query to search for
-   Returns:
-       The search result
-   """
-   print (f"Searching for {query}")
-   return str (tavily.search (query = query))
+class AgentResponse (BaseModel):
+    """Schema for an agent reposnse with answer and sources"""
+    answer:str = Field (description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources to generate the answer")
 
-llm = ChatGroq (model="llama-3.3-70b-versatile")
-tools = [search]
-agent = create_react_agent (model = llm, tools = tools)
+llm = ChatGroq(
+    model="meta-llama/llama-4-scout-17b-16e-instruct"
+)
+tools = [TavilySearch ()]
+
+agent = create_react_agent(
+    model=llm,
+    tools=tools,
+    AgentResponse = AgentResponse
+)
 
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke ({"messages":[HumanMessage(content = "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and their details")]})
-    print (result)
+    result = agent.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="Weather in japan right now"
+                )
+            ]
+        }
+    )
+    print(result)
+
 if __name__ == "__main__":
     main()
